ac5f015250303e114a7686a4ecef8663ec9217d2
Anne Saber <iarabejo3@gmail.com>
2022-03-18 13:45:59 -0500

    Fixed the gradient animation in splash css.

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,19 +1,24 @@
-var inputName = document.querySelector("#name");
-var inputUsername = document.querySelector("#username");
-var inputEmail = document.querySelector("#email");
-var inputPassword = document.querySelector("#password");
-var submitBtn = document.querySelector("#btn");
-var submissionResponse = document.querySelector("#response");
+var form = document.getElementsByTagName("form");
+var inputName = document.getElementById("#name");
+var inputUsername = document.getElementById("#username");
+var inputEmail = document.getElementById("#email");
+var inputPassword = document.getElementById("#password");
+var submitBtn = document.getElementById("#btn");
+var submissionResponse = document.getElementById("#response");
 
 // Action to be performed on click store in named function
-function showResponse(event) {
-  // Prevent default action
-  event.preventDefault();
+// function showResponse(event) {
+//   // Prevent default action
+//   event.preventDefault();
  
-  var response = " Thank you for your submission. ";
+//   var response = " Thank you for your submission. ";
   
-  submissionResponse.textContent = response;
-}
+//   submissionResponse.textContent = response;
+// }
   
 // Add listener to submit element
-submitBtn.addEventListener("click", showResponse);
+// submitBtn.addEventListener("click", showResponse);
+form.addEventListener("submitBtn", (event) => {
+  event.preventDefault();
+  alert("Thank you for logging in!");
+});
