8d8d16311f776afb3ae59f04eaea42fb8fbd388d
Anne Saber <iarabejo3@gmail.com>
2022-03-19 02:18:32 -0500

    Modal confirmation at login page. Fixed media queries.

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,24 +1,41 @@
-var form = document.getElementsByTagName("form");
-var inputName = document.getElementById("#name");
-var inputUsername = document.getElementById("#username");
-var inputEmail = document.getElementById("#email");
-var inputPassword = document.getElementById("#password");
-var submitBtn = document.getElementById("#btn");
-var submissionResponse = document.getElementById("#response");
+var form = document.getElementById(".form-container");
+var thankYouSection = document.getElementById("#confirmationModal");
+var submitBtn = document.querySelector("submit");
 
-// Action to be performed on click store in named function
-// function showResponse(event) {
-//   // Prevent default action
-//   event.preventDefault();
- 
-//   var response = " Thank you for your submission. ";
-  
-//   submissionResponse.textContent = response;
-// }
-  
-// Add listener to submit element
-// submitBtn.addEventListener("click", showResponse);
-form.addEventListener("submitBtn", (event) => {
-  event.preventDefault();
-  alert("Thank you for logging in!");
-});
+
+const inputName = document.querySelector("name");
+const inputUsername = document.querySelector("username");
+const inputEmail = document.querySelector("email");
+const inputPassword = document.querySelector("password");
+
+
+
+submitBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+  if (isFormValid){
+    form.removal();
+    thankYouSection.classList.remove("hidden");
+  }
+})
+
+inputName.addEventListener('input', () => {
+  validateInputs
+})
+
+
+submitBtn.addEventListener("submit", function() {
+
+  form.classList.remove("active");
+  thankYouSection.classList.add("active");
+
+  thankYouSection.textContent = response;
+
+  validateInputs();
+})
+
+
+
+
+
+
