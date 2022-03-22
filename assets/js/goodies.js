e07d8c7c0535d753869607398627dc07498dae76
Baeonetta <victor.m.kennedy@gmail.com>
2022-03-22 17:14:36 -0500

    added local storage functionality

--- assets/js/goodies.js
+++ assets/js/goodies.js
@@ -33,19 +33,19 @@ function loadFromLocalStorage(){
     images.forEach(displayImgData);
   }
 }
-
+// Grabs the image from upload and stores them into local storage
 function addImage(imgData){
   imagesObject.push(imgData);
   displayNumberOfImgs();
   localStorage.setItem("images", JSON.stringify(imagesObject));
 }
-
+// Places the info on the page, added some custom stylings
 function displayImgData(imgData){
   var span = document.createElement('span');
-  span.innerHTML = '<img class="thumb" src="' + imgData + '"/>';
+  span.innerHTML = '<div class="col-xl-3 col-md-3 col-xxl-3"><img id="gifies"class="img-responsive img-thumbnail" src="' + imgData + '"/></div>';
   document.getElementById('list').insertBefore(span, null);
 }
-
+// Dispalys all the info Dynamically on the page 
 function displayNumberOfImgs(){
   if(imagesObject.length > 0){
 
@@ -54,7 +54,7 @@ function displayNumberOfImgs(){
     document.getElementById("deleteImgs").style.display = "inline";
     
   } else {
-    document.getElementById("state").innerHTML = "No images stored in your browser.";
+    document.getElementById("state").innerHTML = "Nope, nothing in your local storage.";
     document.getElementById("deleteImgs").style.display = "none";
   }
   
@@ -68,6 +68,6 @@ function deleteImages(){
   document.getElementById('list').innerHTML = "";
 }
 
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+document.getElementById('formFileLg').addEventListener('change', handleFileSelect, false);
 document.getElementById('deleteImgs').addEventListener("click", deleteImages);
 loadFromLocalStorage();
